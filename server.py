5ea169cd4375a92c7482ef19b132e82cc8c50aff
Jelle de Vries <jelle_de_vriesie@hotmail.com>
2018-05-30 11:16:17 +0200

    Route change

--- server.py
+++ server.py
@@ -5,16 +5,15 @@ import login as login
 
 app = Flask(__name__)
 
+@app.route('/profile')
+def profile():
+    if not login.check_login():
+        print("Not logged in!", file=sys.stderr)
+        return "Not logged in!"
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def route(path):
-    # Hier kun je een login check doen. Het url pad wordt meegegeven in path #
-    if path == 'profile':
-        if not login.check_login():
-            print("Not logged in!", file=sys.stderr)
-            return "Not logged in!"
-
     return render_template('index.html')
 
 
